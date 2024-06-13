6613b52affdccb92780c4b04d491fb8c0ad893b2
Leonhard Riedisser <leo008180@googlemail.com>
2024-06-13 10:53:31 +0200

    Move command parsing into Optparse module

--- src/Config/Optparse.hs
+++ src/Config/Optparse.hs
@@ -1,7 +1,8 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Config.Optparse (parseConfigFromOptions) where
 
-import Config.Types (ApiToken (ApiToken), BaseUrl (BaseUrl), GroupId (GroupId), PartialConfig (PartialConfig))
-import qualified Config.Types
+import Config.Types
 import qualified Data.Semigroup as S (First (..))
 import Network.URI (URI, parseAbsoluteURI)
 import Options.Applicative
@@ -24,8 +25,47 @@ parser =
     <$> optionalParser (option (GroupId <$> auto) (long "group-id" <> help "set the ID of the group to look at" <> metavar "ID"))
     <*> optionalParser (option (BaseUrl <$> eitherReader f) (long "base-url" <> help "Base URL of the Gitlab instance (e.g. `https://gitlab.com/`)" <> metavar "URL"))
     <*> optionalParser (option (ApiToken <$> auto) (long "api-token" <> help "API Token to use for authorizing requests against the Gitlab API. `api` scope is required." <> metavar "TOKEN"))
-    <*> (Just . S.First <$> Config.Types.parser)
+    <*> (Just . S.First <$> commandParser)
   where
     f :: String -> Either String URI
     f s = maybeToRight ("\"" <> s <> "\" is not a valid absolute URI") (parseAbsoluteURI s)
     optionalParser p = fmap S.First <$> optional p
+
+commandParser :: Parser Command
+commandParser =
+  hsubparser
+    $ mconcat
+      [ command "show-branches" (info (pure ShowBranches) (progDesc "show branches")),
+        command "show-projects" (info (pure ShowProjects) (progDesc "show projects")),
+        command "list-all-projects-meta" (info (pure ListAllProjectsMeta) (progDesc "list all the projects for all groups that are visible for the provided API token in (almost) meta compatible JSON format")),
+        command "list-projects-meta" (info (pure ListProjectsMeta) (progDesc "list the projects for the given group in (almost) meta compatible JSON format")),
+        command "enable-source-branch-deletion" (info (EnableSourceBranchDeletionAfterMerge <$> executionParser) (progDesc "enable source branch deletion after merge for all projects")),
+        command "enable-all-discussions-must-be-resolved-for-merge-requirement" (info (EnableAllDiscussionsMustBeResolvedForMergeRequirement <$> executionParser) (progDesc "enable the requirement that all discussions must be resolved for an MR to be merged for all projects")),
+        command "enable-successful-pipeline-for-merge-requirement" (info (EnableSuccessfulPipelineForMergeRequirement <$> executionParser) (progDesc "enable the requirement that there must be a successful pipeline for an MR to be merged for all projects. CAUTION: Use with care, might not do what you want in projects without pipelines")),
+        command "show-schedules" (info (pure ShowSchedules) (progDesc "show schedules")),
+        command "show-merge-requests" (info (pure ShowMergeRequests) (progDesc "show projects with and without enabled merge requests, list merge requests")),
+        command "count-deployments" (info (CountSuccessfulDeployments <$> argument (Year <$> auto) (metavar "YEAR")) (progDesc "count the number of successful deployments per project (a successful push pipeline on the master branch is counted as a deployment)")),
+        command "set-merge-method-to-fast-forward" (info (SetMergeMethodToFastForward <$> executionParser) (progDesc "Set the merge method for all projects to \"Fast Forward\"")),
+        command "update-merge-requests" (info mergeRequestUpdatActionParser (progDesc "Update all MRs from a given user that match a given condition with a given command"))
+      ]
+
+mergeRequestUpdatActionParser :: Parser Command
+mergeRequestUpdatActionParser =
+  UpdateMergeRequests
+    <$> argument (eitherReader f) (metavar "ACTION" <> help "The action to perform. Must be one of \"rebase\", \"merge\"")
+    <*> option (AuthorIs <$> auto) (short 'u' <> long "user-id" <> help "only MRs opened by the user with this ID are taken into account" <> metavar "ID")
+    <*> optional (SearchTerm <$> strOption (short 's' <> long "search" <> help "Optional. a string that must appear in the MR description or title" <> metavar "TXT"))
+    <*> executionParser
+  where
+    f :: String -> Either String MergeRequestUpdateAction
+    f "rebase" = Right Rebase
+    f "merge" = Right Merge
+    f s = Left $ s <> " is not a valid MergeRequestUpdateAction"
+
+executionParser :: Parser Execution
+executionParser =
+  ( \case
+      False -> DryRun
+      True -> Execute
+  )
+    <$> switch (long "execute" <> short 'x' <> help "whether to actually change the config via the API. By default, only a dry run will be performed")
--- src/Config/Types.hs
+++ src/Config/Types.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
 
 module Config.Types
   ( Config (..),
@@ -16,8 +15,6 @@ module Config.Types
     SearchTerm (..),
     Year (..),
     Execution (..),
-    commandParser,
-    parser,
   )
 where
 
@@ -87,53 +84,3 @@ newtype AuthorIs = AuthorIs Int deriving stock (Show)
 newtype SearchTerm = SearchTerm String deriving stock (Show)
 
 newtype Year = Year Int deriving stock (Show)
-
-commandParser :: IO Command
-commandParser =
-  execParser
-    $ info
-      (helper <*> parser)
-      ( fullDesc
-          <> progDesc "gitlab-helper"
-          <> header "gitlab-helper - a collection of utilities for dealing with a load of projects in Gitlab"
-          <> footer "For the commands that are not read-only, use \"-x\" to make them actually do stuff"
-      )
-
-parser :: Parser Command
-parser =
-  hsubparser
-    $ mconcat
-      [ command "show-branches" (info (pure ShowBranches) (progDesc "show branches")),
-        command "show-projects" (info (pure ShowProjects) (progDesc "show projects")),
-        command "list-all-projects-meta" (info (pure ListAllProjectsMeta) (progDesc "list all the projects for all groups that are visible for the provided API token in (almost) meta compatible JSON format")),
-        command "list-projects-meta" (info (pure ListProjectsMeta) (progDesc "list the projects for the given group in (almost) meta compatible JSON format")),
-        command "enable-source-branch-deletion" (info (EnableSourceBranchDeletionAfterMerge <$> executionParser) (progDesc "enable source branch deletion after merge for all projects")),
-        command "enable-all-discussions-must-be-resolved-for-merge-requirement" (info (EnableAllDiscussionsMustBeResolvedForMergeRequirement <$> executionParser) (progDesc "enable the requirement that all discussions must be resolved for an MR to be merged for all projects")),
-        command "enable-successful-pipeline-for-merge-requirement" (info (EnableSuccessfulPipelineForMergeRequirement <$> executionParser) (progDesc "enable the requirement that there must be a successful pipeline for an MR to be merged for all projects. CAUTION: Use with care, might not do what you want in projects without pipelines")),
-        command "show-schedules" (info (pure ShowSchedules) (progDesc "show schedules")),
-        command "show-merge-requests" (info (pure ShowMergeRequests) (progDesc "show projects with and without enabled merge requests, list merge requests")),
-        command "count-deployments" (info (CountSuccessfulDeployments <$> argument (Year <$> auto) (metavar "YEAR")) (progDesc "count the number of successful deployments per project (a successful push pipeline on the master branch is counted as a deployment)")),
-        command "set-merge-method-to-fast-forward" (info (SetMergeMethodToFastForward <$> executionParser) (progDesc "Set the merge method for all projects to \"Fast Forward\"")),
-        command "update-merge-requests" (info mergeRequestUpdatActionParser (progDesc "Update all MRs from a given user that match a given condition with a given command"))
-      ]
-
-mergeRequestUpdatActionParser :: Parser Command
-mergeRequestUpdatActionParser =
-  UpdateMergeRequests
-    <$> argument (eitherReader f) (metavar "ACTION" <> help "The action to perform. Must be one of \"rebase\", \"merge\"")
-    <*> option (AuthorIs <$> auto) (short 'u' <> long "user-id" <> help "only MRs opened by the user with this ID are taken into account" <> metavar "ID")
-    <*> optional (SearchTerm <$> strOption (short 's' <> long "search" <> help "Optional. a string that must appear in the MR description or title" <> metavar "TXT"))
-    <*> executionParser
-  where
-    f :: String -> Either String MergeRequestUpdateAction
-    f "rebase" = Right Rebase
-    f "merge" = Right Merge
-    f s = Left $ s <> " is not a valid MergeRequestUpdateAction"
-
-executionParser :: Parser Execution
-executionParser =
-  ( \case
-      False -> DryRun
-      True -> Execute
-  )
-    <$> switch (long "execute" <> short 'x' <> help "whether to actually change the config via the API. By default, only a dry run will be performed")
