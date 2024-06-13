0461a83d913e7d5f23c7516d345408c3abbe5b95
Leonhard Riedisser <leo008180@googlemail.com>
2024-06-13 11:34:42 +0200

    Add test for mr action help text

--- test/HelpTextSpec.hs
+++ test/HelpTextSpec.hs
@@ -9,10 +9,17 @@ import Test.Syd
 
 spec :: Spec
 spec = do
-  it "provides a help text" $ do
-    let res = execParserPure defaultPrefs (parserInfo) ["--help"]
+  it "provides a help text for the main program" $ do
+    let res = execParserPure defaultPrefs parserInfo ["--help"]
     case res of
       Failure (ParserFailure f) -> do
         let (parserHelp, _, _) = f "gitlab-helper"
         goldenShowInstance "test_resources/help-text.txt" parserHelp
+      _ -> undefined
+  it "provides a help text for working with MRs" $ do
+    let res = execParserPure defaultPrefs parserInfo ["update-merge-requests", "--help"]
+    case res of
+      Failure (ParserFailure f) -> do
+        let (parserHelp, _, _) = f "gitlab-helper"
+        goldenShowInstance "test_resources/help-text-mrs.txt" parserHelp
       _ -> undefined
