a5920fe0c69d7bd13a9175ad3bca494e8c7a1fe6
Leonhard Riedisser <leo008180@googlemail.com>
2024-06-13 10:53:31 +0200

    Remove redundant imports

--- src/App.hs
+++ src/App.hs
@@ -6,10 +6,8 @@ module App (run) where
 import Branches (showBranchesForGroup)
 import Config.Config (parseConfigOrDie)
 import Config.Types
-import Effects (ProjectId (..))
 import Interpreters
 import MergeRequests
-import Pipelines
 import Projects
 import Relude
 import Schedules (showSchedulesForGroup)
