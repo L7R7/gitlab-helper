3393a7eaf3327c3e0733fb4a83feb6e41a2e9291
Leonhard Riedisser <leo008180@googlemail.com>
2024-06-19 14:23:47 +0200

    Fix output on missing command

--- test/HelpTextSpec.hs
+++ test/HelpTextSpec.hs
@@ -11,11 +11,11 @@ spec :: Spec
 spec = do
   it "provides a help text for the main program" $ parserGoldenTest ["--help"] "test_resources/help-text.txt"
   it "provides a help text for working with MRs" $ parserGoldenTest ["update-merge-requests", "--help"] "test_resources/help-text-mrs.txt"
-  it "returns a useful output when no command is provided" $ parserGoldenTest [] "test_resources/help-text-empty.txt"
+  it "returns a useful output when no command is provided" $ parserGoldenTest [] "test_resources/help-text.txt"
 
 parserGoldenTest :: [String] -> FilePath -> GoldenTest String
 parserGoldenTest args fp = do
-  let res = execParserPure defaultPrefs parserInfo args
+  let res = execParserPure parserPrefs parserInfo args
   case res of
     Failure (ParserFailure f) -> do
       let (parserHelp, _, _) = f "gitlab-helper"
