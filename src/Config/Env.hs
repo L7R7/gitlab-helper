e9be82bfc3790539572799e2e65e99f4755bda12
Leonhard Riedisser <leo008180@googlemail.com>
2024-06-25 16:11:50 +0200

    Improve names of environment variables
    Closes #13

--- src/Config/Env.hs
+++ src/Config/Env.hs
@@ -20,10 +20,10 @@ partialConfigParser :: E.Parser E.Error (PartialConfig (Compose Maybe S.First))
 partialConfigParser =
   btraverse maybeFirstParser
     $ PartialConfig
-      (Id <$> E.var E.auto "HB_GROUP_ID" (E.help "ID of the Gitlab group for which the hooks should be set"))
-      (BaseUrl <$> E.var (absoluteURIFromEnv <=< E.nonempty) "HB_BASE_URL" (E.help "Base URL of the Gitlab instance (e.g. `https://gitlab.com/`)"))
-      (ApiToken <$> E.var (E.str <=< E.nonempty) "HB_API_TOKEN" (E.help "API Token to use for authorizing requests against the Gitlab API. `api` scope is required."))
-      (fmap Id <$> E.var (traverse E.auto <=< E.splitOn ',') "HB_EXCLUDE_PROJECTS" (E.help "List of projects to exclude"))
+      (Id <$> E.var E.auto "GLH_GROUP_ID" (E.help "ID of the Gitlab group you're working in"))
+      (BaseUrl <$> E.var (absoluteURIFromEnv <=< E.nonempty) "GLH_BASE_URL" (E.help "Base URL of the Gitlab instance (e.g. `https://gitlab.com/`)"))
+      (ApiToken <$> E.var (E.str <=< E.nonempty) "GLH_API_TOKEN" (E.help "API Token to use for authorizing requests against the Gitlab API. `api` scope is required."))
+      (fmap Id <$> E.var (traverse E.auto <=< E.splitOn ',') "GLH_EXCLUDE_PROJECTS" (E.help "List of projects to exclude"))
       empty
 
 maybeFirstParser :: E.Parser E.Error a -> E.Parser E.Error (Compose Maybe S.First a)
